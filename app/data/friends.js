1c6f6b48c08dda7866326c953d75c536362010c4
Nate Price <nate.e.price@gmail.com>
2019-10-26 11:19:32 -0500

    css not working on localhost

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+
+var friendsArray = [
+    {
+        "name": "Lionel Messi",
+        "photo": "https://images.app.goo.gl/mrQ19spqtMTfTRyS7",
+        "scores": ["5","1","4","3","5","1","3","4","1","4"]
+    },
+    {
+        "name": "Cristiano Ronaldo",
+        "photo": "https://images.app.goo.gl/xFMfb8wVo8mtTV8f9",
+        "scores": ["1","5","2","3","1","5","3","2","5","2"]
+    },
+    {
+        "name": "Christian Pulisic",
+        "photo": "https://images.app.goo.gl/DVQ7C6naJbX6SFvAA",
+        "scores": ["2","3","3","3","2","3","2","3","2","3"]
+    },
+    {
+        "name": "Alex Morgan",
+        "photo": "https://images.app.goo.gl/D4nqzJeZa32GgcNU9",
+        "scores": ["5","2","4","2","5","1","2","3","1","4"]
+    },
+    {
+        "name": "Christen Press",
+        "photo": "https://images.app.goo.gl/nsrFU2aV4CtHkYPv9",
+        "scores": ["5","3","2","2","5","4","4","2","5","4"]
+    },
+    {
+        "name": "Sydney Leroux",
+        "photo": "https://images.app.goo.gl/NRiU5bPZGs18Hj4L6",
+        "scores": ["3","4","4","3","5","5","1","2","5","2"]
+    }
+]
+
+module.exports = friendsArray;
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var path = require('path');
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;; 
+
+
+app.use(cors()); //saves us from having to do access-control-allow-origin
+app.use(bodyParser.json());
+
+//importing routes
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app, path);
+
+
+app.listen(PORT, ()=> {
+    console.log(`Connected on port: ${PORT}`);
+});
